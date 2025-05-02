cda5b1a299e68c36576fd8304e77373f76dab6de
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2025-05-02 09:48:30 +0400

    chore: add auto-save plugin

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,22 @@
+return {
+  "Pocco81/auto-save.nvim",
+  config = function()
+    require("auto-save").setup {
+      enabled = true,
+      execution_message = {
+        message = function() return ("AutoSave: " .. vim.fn.strftime("%H:%M:%S")) end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      events = {"InsertLeave", "TextChanged"},
+      conditions = {
+        exists = true,
+        filename_is_not = {},
+        filetype_is_not = {},
+        modifiable = true,
+      },
+      write_all_buffers = false,
+      debounce_delay = 135,
+    }
+  end
+}
