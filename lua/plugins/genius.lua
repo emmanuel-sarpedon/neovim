47970717f378490ebca948a70f19516b14923d17
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2025-06-17 09:12:26 +0400

    chore: disable ai code autocompletion

--- lua/plugins/genius.lua
+++ lua/plugins/genius.lua
@@ -1,20 +1,21 @@
-return {
-  "archibate/genius.nvim",
-  requires = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("genius").setup({
-      default_bot = "openai",
-      config_openai = {
-        api_key = os.getenv("OPENAI_API_KEY"),
-      },
-      completion_delay_ms = 500, -- miliseconds before completion triggers, set this to -1 to disable and only allows manual trigger
-      infill_options = {
-        max_tokens = 400,
-        temperature = 0.3,
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   "archibate/genius.nvim",
+--   requires = {
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--   },
+--   config = function()
+--     require("genius").setup({
+--       default_bot = "openai",
+--       config_openai = {
+--         api_key = os.getenv("OPENAI_API_KEY"),
+--       },
+--       completion_delay_ms = 500, -- miliseconds before completion triggers, set this to -1 to disable and only allows manual trigger
+--       infill_options = {
+--         max_tokens = 400,
+--         temperature = 0.3,
+--       },
+--     })
+--   end,
+-- }
